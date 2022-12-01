9b2e8cbd9930e3a1bbf82159281cfcb693ebb97c
mezhanglei <496623925@qq.com>
2022-12-01 14:38:23 +0800

    update

--- src/components/loading-animation/circle-loading.js
+++ src/components/loading-animation/circle-loading.js
@@ -3,16 +3,16 @@ import "./circle-loading.less";
 // 圆形轨迹动画
 export default function CircleLoading() {
 
-    return (
-        <div className="circle-loading">
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-    );
+  return (
+    <div className="circle-loading">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
 };
